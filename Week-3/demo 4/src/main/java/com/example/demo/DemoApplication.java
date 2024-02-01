cbcbcfa36ed7fad58d537e35c8c74d08d7aa23b6
cindyychao <152840216+cindyychao@users.noreply.github.com>
2024-02-01 14:12:17 +0800

    Update DemoApplication.java with complete code

--- Week-3/demo 4/src/main/java/com/example/demo/DemoApplication.java
+++ Week-3/demo 4/src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,8 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
