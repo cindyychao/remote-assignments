9c7c3ebc66e5b17585405811b2332446904953c3
cindyychao <152840216+cindyychao@users.noreply.github.com>
2024-02-06 23:31:23 +0800

    Delete Week-4/Assignment-3 2 directory

--- Week-4/Assignment-3 2/src/main/java/com/example/Assignment3/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Assignment3.repository;
-
-import com.example.Assignment3.entity.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-    boolean existsByEmail(String email);
-    User findByEmailAndPassword(String email, String password);
-}
